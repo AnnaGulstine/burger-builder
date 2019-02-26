b8824c9f8c86435d802ed4b01735b7e9dee1d6bd
AnnaGulstine <annagulstine@gmail.com>
2019-02-25 22:45:16 -0600

    add top navigation

---
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './NavigationItems.css';
+import NavigationItem from '../../../../components/UI/Navigation/NavigationItem/NavigationItem';
+
+const navigationItems = () => {
+  return (<ul className={classes.Navigation}>
+      <NavigationItem link="/" active>Burger Builder</NavigationItem>
+      <NavigationItem >Checkout</NavigationItem>
+    </ul>)
+}
+
+export default navigationItems;
--- src/components/UI/Navigation/Toolbar/Toolbar.js
+++ src/components/UI/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../../../components/UI/Logo/Logo';
+import NavigationItems from '../../../../components/UI/Navigation/NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
   return <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo></Logo>
+    <Logo />
     <nav>
+      <NavigationItems />
     </nav>
   </header>
 }
